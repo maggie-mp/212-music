7b479ae1f99313cd9f7c5c43e2debdeaf14ab87c
maggie-mp <magsmcperry@gmail.com>
2022-09-19 19:34:24 +1200

    trying to link database to website

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 
+MENUDB = 'music.db'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    db = sqlite3.connect(MENUDB)
+    print(db)
+
+    music = []
+    cur = db.execute('SELECT * FROM music')
+    for row in cur:
+        music.append(list(row))
+    db.close()
+
+    return render_template('index.html',
+            )
