0ee65594578f017154902e6fa848b689a032c094
maggie-mp <magsmcperry@gmail.com>
2022-09-22 18:22:31 +1200

    linked database
    :)

--- run.py
+++ run.py
@@ -5,16 +5,19 @@ app = Flask(__name__)
 
 MENUDB = 'music.db'
 
+
+
 @app.route('/')
 def index():
     db = sqlite3.connect(MENUDB)
     print(db)
 
     music = []
-    cur = db.execute('SELECT * FROM music')
+    cur = db.execute('SELECT * FROM top50')
     for row in cur:
         music.append(list(row))
     db.close()
 
-    return render_template('index.html',
-            )
+    return render_template('index.html', 
+        disclaimer='This website is for folks living in Aotearoa, New Zealand | Designed and Coded by Maggie McMillan-Perry',
+        music=music)
