b72dae4696011f71916570a9d17c80be3e52df49
maggie-mp <magsmcperry@gmail.com>
2022-10-01 14:31:20 +1300

    fixed weird gaps
    - got bars to reflect popularity
    - got rid of the weird gaps between the bars
    - yet to work out how to show the different info for each hover

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def index():
     print(db)
 
     music = []
-    cur = db.execute ('SELECT * from [top50] limit 10')
+    cur = db.execute ('SELECT * from [top50] limit 50')
     for row in cur:
         music.append(list(row))
     db.close()
--- static/js.js
+++ static/js.js
@@ -2,7 +2,7 @@ document.getElementById('chart').innerHTML = '';
 
 let bar = document.createElement('div');
 bar.className = 'bar';
-bar.style.height = '300px';
+bar.style.height = '83px';
 bar.style.backgroundColor = '#005F73';
 document.getElementById('chart').appendChild(bar);
 
