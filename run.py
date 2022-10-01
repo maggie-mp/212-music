1f5195831fda49a68a256c5a29d960eab5cad341
maggie-mp <magsmcperry@gmail.com>
2022-10-01 16:04:02 +1300

    attepted to fix database issue

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def index():
     print(db)
 
     music = []
-    cur = db.execute ('SELECT * from [top50] limit 50')
+    cur = db.execute ('SELECT * from [top50] limit 90')
     for row in cur:
         music.append(list(row))
     db.close()
--- static/js.js
+++ static/js.js
@@ -7,15 +7,14 @@ bar.style.backgroundColor = '#005F73';
 document.getElementById('chart').appendChild(bar);
 
 function showSix() {
+
             var element = document.getElementById("info");
              
             if(element.style.visibility == "hidden"){
-                //element.style.display = "block";
                 element.style.visibility = "visible";
             } 
 
             else {
-                //element.style.display = "none";
                 element.style.visibility = "hidden";
             }   
         }
